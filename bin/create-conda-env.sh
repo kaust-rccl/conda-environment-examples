b5508456958384647a48ef64232aa951cbd9cdda
pughdr <david.pugh@kaust.edu.sa>
2020-03-11 15:49:32 +0300

    Working environment creation

---
+++ bin/create-conda-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash --login
+
+
+# create the conda environment
+export ENV_PREFIX=$PWD/env
+conda env create --prefix $ENV_PREFIX --file environment.yml --force
+conda activate $ENV_PREFIX
+. postBuild
