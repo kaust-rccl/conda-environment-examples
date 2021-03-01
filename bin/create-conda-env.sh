c624c4041335e8d8b13e009d5f6d9dd13fac0215
ibex-training <ibex-training@kaust.edu.sa>
2021-03-01 18:55:46 +0300

    Fixed environment config files

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -1,4 +1,17 @@
 #!/bin/bash --login
 
+set -e
+
+# creates the conda environment
 PROJECT_DIR=$PWD
 conda env create --prefix $PROJECT_DIR/env --file $PROJECT_DIR/environment.yml --force
+
+# activate the conda env before installing PyTorch Geometric via pip
+conda activate $PROJECT_DIR/env
+TORCH=1.7.0
+CUDA=cu110 
+python -m pip install torch-scatter --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+python -m pip install torch-sparse --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+python -m pip install torch-cluster --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+python -m pip install torch-spline-conv --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+python -m pip install torch-geometric --no-cache-dir
